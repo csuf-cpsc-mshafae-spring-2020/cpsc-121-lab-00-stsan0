42768f7a169b9c8a4aa60a6b29702cda2728b9f1
stsan0 <stsan@csu.fullerton.edu>
2020-01-23 15:47:36 -0800

    did things and yeah

--- part-2/hello_world.cpp
+++ part-2/hello_world.cpp
@@ -6,6 +6,14 @@
 using namespace std;
 
 int main(int argc, char *argv[]){
-  cout << "Hi mom, this is my friend." << argv[1] << ".\n";
+  if (argv[2]>0){
+    for (int repeat = 0;repeat < argv[2];repeat++){
+      cout << "Hi" << argv[1] << ".\n";
+    }
+  }
+  else {
+    cout << "Too few arguments.\n";
+  }
+
   return 0;
 }
