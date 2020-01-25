5323f2b4e5194dbeac231ce39b722b49c3bbd742
stsan0 <54484048+stsan0@users.noreply.github.com>
2020-01-24 16:07:53 -0800

    added variables for testing

--- part-2/hello_world.cpp
+++ part-2/hello_world.cpp
@@ -6,13 +6,18 @@
 using namespace std;
 
 int main(int argc, char *argv[]){
-  if (argv[2]>0){
+  argv[2] = 3;
+  string replace;
+  cout << "Replace the first argument.\n";
+  cin >> replace;
+  if (isalpha(argv[1])){
     for (int repeat = 0;repeat < argv[2];repeat++){
       cout << "Hi" << argv[1] << ".\n";
     }
   }
   else {
     cout << "Too few arguments.\n";
+    return 1;
   }
 
   return 0;
