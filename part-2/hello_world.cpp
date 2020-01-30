17e77d97b4fd6eb7c81c9db32d86190f425580e2
stsan0 <54484048+stsan0@users.noreply.github.com>
2020-01-30 08:26:04 -0800

    changes made properly

--- part-2/hello_world.cpp
+++ part-2/hello_world.cpp
@@ -6,10 +6,6 @@
 using namespace std;
 
 int main(int argc, char *argv[]){
-  argv[2] = 3;
-  string replace;
-  cout << "Replace the first argument.\n";
-  cin >> replace;
   if (isalpha(argv[1])){
     for (int repeat = 0;repeat < argv[2];repeat++){
       cout << "Hi" << argv[1] << ".\n";
